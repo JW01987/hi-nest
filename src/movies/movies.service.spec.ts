a4c3cbc6052c69c5a31a2747c967f28630f03988
Jean <etnpfj@gmail.com>
2023-07-28 14:37:13 +0900

    test: 유닛 테스트 완료

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,7 +1,9 @@
+import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import { Server } from 'http';
 import { MoviesService } from './movies.service';
 
-describe('MoviesService', () => {
+describe('MoviesService 테스트', () => {
   let service: MoviesService;
 
   beforeEach(async () => {
@@ -14,5 +16,95 @@ describe('MoviesService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('getAll() 테스트', () => {
+    it('should be Array', () => {
+      const result = service.getAll();
+      expect(result).toBeInstanceOf(Array);
+    });
+  });
+
+  describe('getOne() 테스트', () => {
+    it('should be return Movie data', () => {
+      service.create({
+        title: 'test',
+        year: 2020,
+        genres: ['test', 'test2'],
+      });
+      const movie = service.getOne(1);
+      expect(movie).toBeDefined();
+      expect(movie.id).toEqual(1);
+    });
+    it('should throw error', () => {
+      try {
+        service.getOne(999);
+      } catch (err) {
+        expect(err).toBeInstanceOf(NotFoundException);
+      }
+    });
+  });
+
+  describe('delete() 테스트', () => {
+    it('delete a movie', () => {
+      service.create({
+        title: 'test',
+        year: 2020,
+        genres: ['test', 'test2'],
+      });
+      const beforeMovie = service.getAll().length;
+      service.deleteOne(1);
+      const afterMovie = service.getAll().length;
+      expect(afterMovie).toBeLessThan(beforeMovie);
+    });
+    it('should throw error', () => {
+      try {
+        service.deleteOne(999);
+      } catch (err) {
+        expect(err).toBeInstanceOf(NotFoundException);
+      }
+    });
+  });
+
+  describe('create() 테스트', () => {
+    it('should create movie', () => {
+      const beforeMovie = service.getAll().length;
+      service.create({
+        title: 'test',
+        year: 2020,
+        genres: ['test', 'test2'],
+      });
+      const afterMovie = service.getAll().length;
+      expect(afterMovie).toBeGreaterThan(beforeMovie);
+    });
+  });
+
+  describe('update() 테스트', () => {
+    it('should update movie', () => {
+      service.create({
+        title: 'test',
+        year: 2020,
+        genres: ['test', 'test2'],
+      });
+      service.update(1, {
+        title: 'update test',
+      });
+      const movie = service.getOne(1).title;
+      expect(movie).toEqual('update test');
+    });
+    it('should throw 404 error', () => {
+      try {
+        service.create({
+          title: 'test',
+          year: 2020,
+          genres: ['test', 'test2'],
+        });
+        service.update(999, {
+          title: 'update test',
+        });
+      } catch (err) {
+        expect(err).toBeInstanceOf(NotFoundException);
+      }
+    });
   });
 });
